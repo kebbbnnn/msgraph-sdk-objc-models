0a8d6023f5553402bd938ef6302199cf360b4efb
runner <runner@Mac-1675688828494.local>
2023-02-06 13:34:11 +0000

    Version and checksum update for 1.0.1

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
             targets: ["MSGraphClientModels"]),
     ],
     targets: [
-        .binaryTarget(name: "MSGraphClientModels", url: "https://github.com/kebbbnnn/msgraph-sdk-objc-models/releases/download/1.0.0-spm/MSGraphClientModels.xcframework.zip", checksum: "99f5c497fa284b22d0d3ce740cd5801757d01e5060ebe6881463311583303682") 
+        .binaryTarget(name: "MSGraphClientModels", url: "https://github.com/kebbbnnn/msgraph-sdk-objc-models/releases/download/1.0.1-spm/MSGraphClientModels.xcframework.zip", checksum: "f7fb7357ff70a9e0bbe9c3e89c9405a2590276444aaa66205fa73d077fb1af3e") 
     ]
 )
